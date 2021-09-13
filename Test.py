18f07d7e403b9392318180713a4c01c87f4261ba
Amanda <amanda.jonsson@elev.ga.ntig.se>
2021-09-13 13:10:11 +0200

    Update Test.py

---
+++ Test.py
@@ -0,0 +1,14 @@
+print("hej på er EE20")
+print("hejhej)
+
+name = input("Vad heter du?")
+print(f"Hej på dig {name}")
+
+age = input("Hur gammal är du?")
+print(f"Oj så gammal du är, hela {age} år.")
+
+test = 12
+
+summa = tal1 + 12
+
+print(f"Summan blir {summa}")
